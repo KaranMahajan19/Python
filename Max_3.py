fe551c75448b655dfab1e19cca56c09b2e2a0841
Karan_Mahajan <101745035+KaranMahajan19@users.noreply.github.com>
2022-10-02 13:34:18 +0530

    Add files via upload
    Application to find out maximum number in between three Values.

---
+++ Max_3.py
@@ -0,0 +1,28 @@
+# Aplication to Findout Maximum number in between 3 number
+
+def Maximum(No1, No2, No3):
+	if (No1 >= No2):
+		return No1
+	elif (No2 >= No3):
+		return No2
+
+	else:
+		return No3
+
+def main():
+
+	print("Enter first no : ")
+	Value1 = input()
+	
+	print("Enter second no : ")
+	Value2 = input()
+
+	print("Enter third no : ")
+	Value3 = input()
+
+	Ans = Maximum(int(Value1),int(Value2),int(Value3))
+
+	print("Maximum number is : ",Ans)
+
+if __name__ == "__main__":
+	main()
